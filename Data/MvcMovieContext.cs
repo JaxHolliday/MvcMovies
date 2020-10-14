0cb81f650e4a11652949fe6768f43801b1bd819e
Jackson Holliday <jacksonHolliday@users.noreply.github.com>
2020-10-14 14:45:22 -0400

    Update

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Movie.Models;
+
+namespace MVC_Movie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
